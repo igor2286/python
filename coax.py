84c31ee5a28e12706772eacd3d6aecabd44889ad
igor2286 <kedenko123@gmail.com>
2020-09-26 23:10:37 +0300

    Update coax.py

--- coax.py
+++ coax.py
@@ -2,19 +2,15 @@ import pandas as pd
 import os
 
 
-class Creator:
+class WorkWithFile:
     def __init__(self):
         self.FILENAME = input('write the name of file: ') + '.csv'
         self.file = open(self.FILENAME, 'a')
 
-    def create_header(self):
+    def create_headers(self):
         header = ['author_name', 'note', 'rating']
         self.file.writelines(str(header) + os.linesep)
-        self.file.close()
         return 'Header is Created!\nTry to read or write something in file!'
-
-
-class WorkWithFile(Creator):
 
     def dataFrame(self):
         df = pd.read_csv(self.FILENAME, header=0)
@@ -22,12 +18,21 @@ class WorkWithFile(Creator):
         df['rating'] = df['rating'].apply(lambda x: x[1:-1])
         df['author_name'] = df['author_name'].apply(lambda x: x[2:-1])
         df['note'] = df['note'].apply(lambda x: x[2:-1])
-        df['rating'] = df['rating'].apply(pd.to_numeric)
+        df['rating'] = df['rating'].apply(pd.to_numeric, errors='coerce')
+
         return df
 
     def write_to_file(self):
         author = input('Add_author: ')
+        if author != '':
+            author = author
+        else:
+            author = 'NaN'
         note = input('Add_note: ')
+        if note != '':
+            note = note
+        else:
+            note = 'NaN'
         rating = float(input('Add_rating(0.0-1.0): '))
         if 0.0 <= rating <= 1.0:
             rating = rating
@@ -40,7 +45,7 @@ class WorkWithFile(Creator):
                 rating = float(input("Введіть коректні дані:"))
         iterator = [author, note, rating]
         self.file.writelines(str(iterator) + os.linesep)
-        self.file.close()
+
         return 'Add to the file:' + self.FILENAME
 
     def read_file(self):
@@ -57,8 +62,3 @@ class Functions(WorkWithFile):
 
     def avg_rating(self):
         return self.dataFrame().rating.mean()
-
-
-
-a = Creator()
-print(a.create_header())
